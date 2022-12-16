631dbbb031ab6a5c59803a5a8aac447d9c52974a
Kevin Liu <kliu33@buffalo.edu>
2022-12-16 17:31:17 -0500

    finished

--- ninety-nine-cats/app/controllers/cats_controller.rb
+++ ninety-nine-cats/app/controllers/cats_controller.rb
@@ -15,19 +15,32 @@ class CatsController < ApplicationController
     end
 
     def new
-
+        @cat = Cat.new
+        render :new
     end
 
     def create
-
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
     end
 
     def edit
+        @cat = Cat.find(params[:id])
 
+        render :edit
     end
 
     def update
-
+        @cat = Cat.find(params[:id])
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
     end
 
     private
