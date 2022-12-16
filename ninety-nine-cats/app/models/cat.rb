a2ab541acac83d46523ed53400a89cb1a0c62383
Joey Tsui <joeytsui131@gmail.com>
2022-12-16 15:50:57 -0500

    cats

---
+++ ninety-nine-cats/app/models/cat.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+require "date"
+class Cat < ApplicationRecord
+    CAT_COLOR = [:white, :black, :gray, :orange, :blue]
+    SEX = [:M, :F]
+    validates :id, :birth_date, :color, :name, :sex, presence: true
+    validate :inclusion, :birth_date_cannot_be_future
+
+    def inclusion 
+        if !CAT_COLOR.include(color)
+            errors.add(:color, "Not possible")
+        end
+
+        if !SEX.include(sex.upcase)
+            errors.add(:sex, "Not a real a gender")
+        end
+    end
+
+    def birth_date_cannot_be_future
+        if birth_date.year > Date.today.year
+            errors.add(:birth_date, "Not a real date")
+        end
+
+        if birth_date.year == Date.today.year
+            if birth_date.mon > Date.today.month
+                errors.add(:birth_date, "Not a real date")
+            end
+
+            if birth_day.mon == Date.today.mon
+                if birth_date.day > Date.today.day
+                    errors.add(:birth_date, "Not a real date")
+                end
+            end
+        end
+    end
+
+    def age
+        age = Date.today.year - birth_date.year
+    end
+
+    
+end
